9f4c44ab24b3fa2a1a89c49905cfa312f614c5bc
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-12-01 11:56:37 +0700

    Add files via upload

---
+++ AUTOVIEFAMILY1/cfg.php
@@ -0,0 +1,19 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
+
+//dogehero.xyz
+$ck2 = "_data_pop=439-1-1638418090; csrf_cookie_name=62e7fa14b6c866324b0194df943cf802; ci_session=6d0329dba169a63e09d565d397747ec477f19554";
+
+//cryptoaffiliates.store
+$ck1 = 'bitmedia_fid=eyJmaWQiOiI3OTVlYzUzNjQ3MDMyOTM4YjkxY2JkMTAwMWZiZjkxNyIsImZpZG5vdWEiOiIyNzZkOWM5ZDVhMmU0YTc1YmY4ZjQ5NTZkNzdhYWFmYyJ9; _pbjs_userid_consent_data=3524755945110770; csrf_cookie_name=a9218f9332e5577ac330c0014fb851b7; ci_session=c35451d374ff0fbb330b1361da4fbcf29701092d';
+
+//1xbitcoins.com
+$ck3 = "csrf_cookie_name=a79ce87687b88214d78a828a9be4f1f3; bitmedia_fid=eyJmaWQiOiI3OTVlYzUzNjQ3MDMyOTM4YjkxY2JkMTAwMWZiZjkxNyIsImZpZG5vdWEiOiIyNzZkOWM5ZDVhMmU0YTc1YmY4ZjQ5NTZkNzdhYWFmYyJ9; ci_session=2c57ee5c503939f4dc48778a7854e750d6c40879";
+
+//btcbunch.com
+$ck4 = "___nrbic=%7B%22previousVisit%22%3A1638330583%2C%22currentVisitStarted%22%3A1638330583%2C%22sessionId%22%3A%22efc06012-a6dc-4914-91d8-ad024c84a62d%22%2C%22sessionVars%22%3A%5B%5D%2C%22visitedInThisSession%22%3Atrue%2C%22pagesViewed%22%3A1%2C%22landingPage%22%3A%22https%3A//btcbunch.com/%22%2C%22referrer%22%3A%22%22%7D; ___nrbi=%7B%22firstVisit%22%3A1638330583%2C%22userId%22%3A%223cc152a7-c148-47fc-be0b-9675af93572b%22%2C%22userVars%22%3A%5B%5D%2C%22futurePreviousVisit%22%3A1638330583%2C%22timesVisited%22%3A1%7D; compass_uid=3cc152a7-c148-47fc-be0b-9675af93572b; _pbjs_userid_consent_data=3524755945110770; bitmedia_fid=eyJmaWQiOiI3OTVlYzUzNjQ3MDMyOTM4YjkxY2JkMTAwMWZiZjkxNyIsImZpZG5vdWEiOiIyNzZkOWM5ZDVhMmU0YTc1YmY4ZjQ5NTZkNzdhYWFmYyJ9; __gads=ID=b1c714e4913323b4-2288556d51cf0018:T=1638330657:RT=1638330657:S=ALNI_MbbJIXKZPLJLtUZA0_nPcZOMrURgg; csrf_cookie_name=31c4300d7e3dd6e21308805e3ff6a420; AdskeeperStorage=%7B%220%22%3A%7B%7D%2C%22C1220758%22%3A%7B%22page%22%3A2%2C%22time%22%3A1638330783959%7D%7D; _gid=GA1.2.983250019.1638330886; _ga_MTE2VX5MJ9=GS1.1.1638330656.1.1.1638332283.0; _ga=GA1.2.1593914839.1638330657; _gat_gtag_UA_212001089_1=1; ci_session=0f35aq51ne54akebclkjt1dpp1rdahqt";
+
+//free.shiba.limited
+$ck5 = "__gads=ID=61704b6e700c40ff-22e3a26c4fcf00c9:T=1638332656:RT=1638332656:S=ALNI_MbzGGRoQ9F72x0UD3Nv0RSvaQdAcA; csrf_cookie_name=b412f5866c73e1f7d9f029c64f6f94f8; ci_session=bn1o5ap44n910f6on6dtlg3qp7ck4lcp";
+
