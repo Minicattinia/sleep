c715bb7343c96830496ff082db9b9c0b1934c955
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-12-07 21:28:24 +0700

    Add files via upload

---
+++ simplebits1/cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+//Isi USER-AGENT Sesuai Data Kalian
+$user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43';
+
+//Isi Cookie Sesuai Data Kalian
+$cookie = '_ga=GA1.2.1279764657.1638887041; _gid=GA1.2.1059051418.1638887041; _gat=1; sbits_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRSUTJvIiwibmFtZSI6InNub3dkb2xsIiwicm9sZSI6IlVzZXIiLCJpYXQiOjE2Mzg4ODcwNzgsImV4cCI6MTYzOTMxOTA3OH0.E4YM6quVUh73WQrU3wgc7XwgFImobwtSecEVabBPIkU; sbstc_ss=8654207; hideouttv=SIMPLE-dRQ2o';
