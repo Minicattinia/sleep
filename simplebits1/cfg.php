3680b7e44f7ec886dfbcc87a4be33f4ded7b7c05
Nekodoll <37606008+Nekodoll@users.noreply.github.com>
2021-12-07 23:48:05 +0700

    Update cfg.php

--- simplebits1/cfg.php
+++ simplebits1/cfg.php
@@ -1,7 +1,7 @@
 <?php
 
 //Isi USER-AGENT Sesuai Data Kalian
-$user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.43';
+$user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36';
 
 //Isi Cookie Sesuai Data Kalian
-$cookie = '_ga=GA1.2.1279764657.1638887041; _gid=GA1.2.1059051418.1638887041; _gat=1; sbits_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRSUTJvIiwibmFtZSI6InNub3dkb2xsIiwicm9sZSI6IlVzZXIiLCJpYXQiOjE2Mzg4ODcwNzgsImV4cCI6MTYzOTMxOTA3OH0.E4YM6quVUh73WQrU3wgc7XwgFImobwtSecEVabBPIkU; sbstc_ss=8654207; hideouttv=SIMPLE-dRQ2o';
+$cookie = 'sbits_session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImRSUTJvIiwibmFtZSI6InNub3dkb2xsIiwicm9sZSI6IlVzZXIiLCJpYXQiOjE2Mzg4OTA3MTEsImV4cCI6MTYzOTMyMjcxMX0.wTI6hO40tRyak3yvkK-AYKYKjSd8Geur5z1eum5OgBg; sbstc_ss=9833985; hideouttv=SIMPLE-dRQ2o';
